4a1058d5cc0acc80cc0031714a06a333cb7802a2
ANewbie <55935729+AcsessAll@users.noreply.github.com>
2020-05-02 16:48:32 +0800

    Create main.py
    main idea

---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+import tkinter as tk
+from tkinter import * 
+from login import * 
+from LoginPage import *  
+from LoginPage_admin import *
+
+
+root = tk.Tk() 
+login(root)
+root.mainloop()
